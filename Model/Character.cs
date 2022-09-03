59d8caf1015826049b08e2d25715b936bb677df9
Marcrows <marcoslanuza.t@gmail.com>
2022-09-03 14:10:24 -0600

    Component to add characters

---
+++ Model/Character.cs
@@ -0,0 +1,9 @@
+namespace CharacterDB.Models;
+
+public class Character
+{
+    public String Id { get; set; }
+    public String Name { get; set; }
+    public String Gender { get; set; }
+    public int Age { get; set; }
+}
